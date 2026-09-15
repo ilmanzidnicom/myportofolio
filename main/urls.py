8bfa9c0745f99358445a34e1a23eeacd613d6de1
ilmanzidnicom <github@ilmanzidni.com>
2026-09-15 21:51:07 +0700

    feat: Implementasi data delivery dengan JSON, dan memperbarui halaman show_projects

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("projects/", show_projects, name="show_projects"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/add/", create_project, name="create_project"),
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project")
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import *
 from .forms import *
@@ -28,10 +30,30 @@ def show_experience(request):
     return render(request, "experience.html", global_context | context)
 
 def show_projects(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
-        "project_list": Project.objects.all(),
+        "project_list": projects,
+        "title_query": title_query,
     }
     return render(request, "project.html", global_context | context)
+
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
 
 def create_project(request):
     form = ProjectForm(request.POST or None)
@@ -46,3 +68,13 @@ def create_project(request):
     }
 
     return render(request, "projects_form.html", global_context | context)
+
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if request.method == "POST":
+        project.delete()
+        messages.success(request, "Project berhasil dihapus!")
+        return redirect("main:show_projects")
+
+    return redirect("main:show_projects")
